8466d7f74b076c49ff1fc7aa2155fd306561fbc9
Kimpe Andy <andykimpe@gmail.com>
2024-02-26 06:35:49 +0100

    Create copr-Custom.sh

---
+++ php56/copr-Custom.sh
@@ -0,0 +1,5 @@
+#! /bin/sh -x
+wget https://raw.githubusercontent.com/andykimpe/scl-php56/master/php56/php56.spec -O php56.spec
+wget https://github.com/andykimpe/scl-php56/raw/master/php56/macros-build -O macros-build
+wget https://github.com/andykimpe/scl-php56/raw/master/README -O README
+wget https://github.com/andykimpe/scl-php56/raw/master/LICENSE -O LICENSE
