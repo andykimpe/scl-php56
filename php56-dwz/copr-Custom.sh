da69f4901871700b9fb56fd0ed8a9cb19a8bd8c1
Kimpe Andy <andykimpe@gmail.com>
2024-02-26 18:16:20 +0100

    Update copr-Custom.sh

--- php56-dwz/copr-Custom.sh
+++ php56-dwz/copr-Custom.sh
@@ -1,1 +1,9 @@
-
+#! /bin/sh -x
+#use for build on
+# copr.fedorainfracloud.org
+# Mock chroot: fedora-39-x86_64
+# External repositories for build dependencies
+# https://download.copr.fedorainfracloud.org/results/andykimpe/php-scl/fedora-39-x86_64/
+# Build dependencies: scl-utils-build php56-runtime wget
+wget https://github.com/andykimpe/scl-php56/raw/master/php56-binutils/php56-dwz.spec -O php56-dwz.spec
+wget https://mirrors.tuna.tsinghua.edu.cn/centos/7.9.2009/sclo/x86_64/rh/Packages/d/devtoolset-7-dwz-0.12-1.el7.sc1.x86_64.rpm -O devtoolset-7-dwz-0.12-1.el7.sc1.x86_64.rpm
