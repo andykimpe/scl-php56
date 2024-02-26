fba18ef1cbb4fd9eda41d54a43780ffa37cb7018
Kimpe Andy <andykimpe@gmail.com>
2024-02-26 06:36:29 +0100

    Update copr-Custom.sh

--- php56/copr-Custom.sh
+++ php56/copr-Custom.sh
@@ -1,4 +1,10 @@
 #! /bin/sh -x
+#use for build on
+# copr.fedorainfracloud.org
+# Mock chroot: fedora-39-x86_64
+# External repositories for build dependencies
+# https://download.copr.fedorainfracloud.org/results/andykimpe/php-scl/fedora-39-x86_64/
+# Build dependencies: scl-utils-build php56-runtime wget
 wget https://raw.githubusercontent.com/andykimpe/scl-php56/master/php56/php56.spec -O php56.spec
 wget https://github.com/andykimpe/scl-php56/raw/master/php56/macros-build -O macros-build
 wget https://github.com/andykimpe/scl-php56/raw/master/README -O README
