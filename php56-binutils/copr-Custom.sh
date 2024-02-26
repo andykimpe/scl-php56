1f4925bc0e7bf272b91e00f554d87b1f7bdfbf0a
Kimpe Andy <andykimpe@gmail.com>
2024-02-26 17:27:58 +0100

    Update copr-Custom.sh

--- php56-binutils/copr-Custom.sh
+++ php56-binutils/copr-Custom.sh
@@ -6,5 +6,5 @@
 # https://download.copr.fedorainfracloud.org/results/andykimpe/php-scl/fedora-39-x86_64/
 # Build dependencies: scl-utils-build php56-runtime wget
 wget https://github.com/andykimpe/scl-php56/raw/master/php56-binutils/php56-binutils.spec -O php56-binutils.spec
-wget https://mirrors.tuna.tsinghua.edu.cn/centos/7.9.2009/sclo/x86_64/rh/Packages/d/devtoolset-10-annobin-9.23-4.el7.1.x86_64.rpm -O devtoolset-10-annobin-9.23-4.el7.1.x86_64.rpm
-wget https://mirrors.tuna.tsinghua.edu.cn/centos/7.9.2009/sclo/x86_64/rh/Packages/d/devtoolset-10-annobin-annocheck-9.23-4.el7.1.x86_64.rpm -O devtoolset-10-annobin-annocheck-9.23-4.el7.1.x86_64.rpm
+wget https://mirrors.tuna.tsinghua.edu.cn/centos/7.9.2009/sclo/x86_64/rh/Packages/d/devtoolset-7-binutils-2.28-8.el7.sc1.x86_64.rpm -O devtoolset-7-binutils-2.28-8.el7.sc1.x86_64.rpm
+wget https://mirrors.tuna.tsinghua.edu.cn/centos/7.9.2009/sclo/x86_64/rh/Packages/d/devtoolset-7-binutils-devel-2.28-8.el7.sc1.x86_64.rpm -O devtoolset-7-binutils-devel-2.28-8.el7.sc1.x86_64.rpm
