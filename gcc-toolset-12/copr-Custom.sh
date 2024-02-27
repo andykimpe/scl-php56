296e625b0bdcaa175c8030f965e19ad91bb2bc49
Kimpe Andy <andykimpe@gmail.com>
2024-02-27 16:06:51 +0100

    Update copr-Custom.sh

--- gcc-toolset-12/copr-Custom.sh
+++ gcc-toolset-12/copr-Custom.sh
@@ -1,4 +1,9 @@
 #! /bin/sh -x
+#
+#
+#
+#
 wget https://mirror.stream.centos.org/9-stream/AppStream/source/tree/Packages/gcc-toolset-12-12.0-5.el9.src.rpm
 rpm2cpio *.src.rpm | cpio -idmv
-rm -f *.src.rpm
+rm -f *.src.rpm gcc-toolset-12.spec
+wget https://github.com/andykimpe/scl-php56/raw/master/gcc-toolset-12/gcc-toolset-12.spec -O gcc-toolset-12.spec
