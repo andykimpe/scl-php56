6c4a646a317582364f0ad05c14cfe6ab32723d7f
Kimpe Andy <andykimpe@gmail.com>
2024-02-27 16:09:14 +0100

    Update copr-Custom.sh

--- scl-utils/copr-Custom.sh
+++ scl-utils/copr-Custom.sh
@@ -1,10 +1,8 @@
 #! /bin/sh -x
-#use for build on
-# copr.fedorainfracloud.org
-# Mock chroot: fedora-39-x86_64
-# External repositories for build dependencies
-# https://download.copr.fedorainfracloud.org/results/andykimpe/php-scl/fedora-39-x86_64/
-# Build dependencies: scl-utils-build php56-runtime wget
+#! /bin/sh -x
+#wget https://github.com/andykimpe/scl-php56/raw/master/scl-utils/copr-Custom.sh -O copr-Custom.sh
+#bash copr-Custom.sh
+#rm -f copr-Custom.sh
 wget https://raw.githubusercontent.com/andykimpe/scl-php56/master/scl-utils/scl-utils.spec -O scl-utils.spec
 wget https://github.com/sclorg/scl-utils/archive/2.0.3/scl-utils-2.0.3.tar.gz -O scl-utils-2.0.3.tar.gz
 wget https://raw.githubusercontent.com/andykimpe/scl-php56/master/scl-utils/macros.scl-filesystem -O macros.scl-filesystem
