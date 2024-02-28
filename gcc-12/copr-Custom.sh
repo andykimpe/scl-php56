595e42995c359fb938133d60a6f75ac5d5cfdb5e
Kimpe Andy <andykimpe@gmail.com>
2024-02-28 12:18:59 +0100

    Update copr-Custom.sh

--- gcc-12/copr-Custom.sh
+++ gcc-12/copr-Custom.sh
@@ -1,5 +1,5 @@
 #! /bin/sh -x
 wget https://archives.fedoraproject.org/pub/fedora/linux/releases/37/Everything/source/tree/Packages/g/gcc-12.2.1-2.fc37.src.rpm -O gcc-12.2.1-2.fc37.src.rpm
 rpm2cpio gcc-12.2.1-2.fc37.src.rpm | cpio -idmv
-rm -rf gcc-12.2.1-2.fc37.src.rpm
+rm -rf gcc-12.2.1-2.fc37.src.rpm gcc.spec
 wget https://github.com/andykimpe/scl-php56/raw/master/gcc-12/gcc-12.spec -O gcc-12.spec
